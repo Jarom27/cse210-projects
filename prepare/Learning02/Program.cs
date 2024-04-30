32c190d10bbe0d439cfeecd04d7eb7cc6002e642
Jarom Mariscal <jaromero_marz234@outlook.com>
2024-04-30 15:50:18 -0700

    Learning activity finished

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "Microsoft";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2020;
+        job1._endYear = 2024;
+
+        Job job2 = new Job();
+        job2._company = "Apple";
+        job2._jobTitle = "Web developer";
+        job2._startYear = 2015;
+        job2._endYear = 2021;
+
+        // job1.Display();
+        // job2.Display();
+
+        Resume resume = new Resume();
+        resume._name = "Jarom Mariscal";
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+
+        // Console.WriteLine(resume._jobs[0]._jobTitle);
+        // Console.WriteLine(resume._jobs[1]._jobTitle);
+        resume.Display();
+
+
     }
 }
