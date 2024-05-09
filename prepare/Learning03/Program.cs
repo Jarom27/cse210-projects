2c79bcd53dc0bc1ce333c86c4c06abf90c911ad4
Jarom Mariscal <jaromero_marz234@outlook.com>
2024-05-09 12:47:33 -0700

    Learning activity 3 finished

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction number1 = new Fraction();
+        Fraction number2 = new(6);
+        Fraction number3 = new(6, 7);
+
+        Fraction number4 = new(3, 4);
+        Fraction number5 = new(5);
+        Fraction number6 = new(1, 3);
+
+        Console.WriteLine(number1.GetFractionString());
+        Console.WriteLine(number1.GetDecimalValue());
+        Console.WriteLine(number4.GetFractionString());
+        Console.WriteLine(number4.GetDecimalValue());
+        Console.WriteLine(number5.GetFractionString());
+        Console.WriteLine(number5.GetDecimalValue());
+        Console.WriteLine(number6.GetFractionString());
+        Console.WriteLine(number6.GetDecimalValue());
     }
 }
