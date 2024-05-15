1a8d1ec097d08f9c153f179f8ee895085df3d414
Jarom Mariscal <jaromero_marz234@outlook.com>
2024-05-14 19:46:49 -0700

    Learning Activity 4 finished

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment = new Assignment("Samuel Bennet", "Multiplication");
+        MathAssignment mathAssignment = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        WritingAssigment writingAssigment = new WritingAssigment("Mary Waters", "European History", "Causes of the Second World War");
+        Console.WriteLine(assignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+        Console.WriteLine(writingAssigment.GetSummary());
+        Console.WriteLine(writingAssigment.GetWritingInformation());
+
+
     }
 }
